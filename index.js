0741d5022e38e604ae54f267c440fe4d2429bd02
glorious-cyborg-8787 <ajn252@gmail.com>
2020-07-20 16:35:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ array.forEach(element => {
 })
 
 }
-/*
+
 function iterate(array){
   var animals = [1,2,3,4,5,6] 
   array.forEach(callbackfunction);
   return array;
 }
-
+/*
 function doToArray(array, callback){
   array.forEach(callback);
 }*/
